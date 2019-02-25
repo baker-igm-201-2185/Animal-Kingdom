653a3b097c2275735c448ca4764dfde95945d082
Eric <eabigm@gmail.com>
2019-02-25 15:27:52 -0500

    Made animals do tricks

--- Animal Kingdom/Animal Kingdom/Program.cs
+++ Animal Kingdom/Animal Kingdom/Program.cs
@@ -13,6 +13,26 @@ namespace Animal_Kingdom
             Dog myDog = new Dog("Lucky", "Cocker Spaniel", "Black", FoodType.Omnivore);
 
             Cat yourCat = new Cat("Misty", "Scottish Fold", "Brown", FoodType.Carnivore);
+
+            //  Move
+            hedgehog.Move();
+            myDog.Move();
+            yourCat.Move();
+
+            //  Speak
+            hedgehog.Speak();
+            myDog.Speak();
+            yourCat.Speak();
+
+            //  Eat
+            hedgehog.Eat();
+            myDog.Eat();
+            yourCat.Eat();
+
+            //  Play
+            hedgehog.Play();
+            myDog.Play();
+            yourCat.Play();
         }
     }
 }
